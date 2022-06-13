69b5f8d416fdae0bafa87e3b93ab97d9b0a4f68b
Anas Abbas <anastorky2015@gmail.com>
2022-06-13 21:04:02 +0200

    this commit will fail

--- NIDS.cu
+++ NIDS.cu
@@ -20,20 +20,34 @@ using namespace std::chrono;
 using namespace cub;
 CachingDeviceAllocator g_allocator(true);
 #define ull unsigned long long
-__constant__ const int d = 2;
-__constant__ const ull q = 65521;
-__constant__ const int p = 1024;
-__constant__ const ull n = 1 << 18;
-__constant__ const ull m = 13;
-__constant__ const ull mps[] = {7, 31, 127, 8191, 131071, 524287};
-__constant__ const int masksz = 6;
-__constant__ const ull masks[] = {7, 248, 32512, 268402688, 35184103653376ull, 18446708889337462784ull};
-__constant__ const ull nmasks[] = {18446744073709551608ull, 18446744073709551367ull, 18446744073709519103ull, 18446744073441148927ull, 18446708889605898239ull, 35184372088831ull};
-__constant__ const ull shifts[] = {3, 5, 7, 13, 17, 19};
-__constant__ const ull cumShifts[] = {0, 3, 8, 15, 28, 45};
-__constant__ const ull ds[] = {2, 3, 5, 11, 13, 17};
-__constant__ const ull HTSZ = 1 << 18;
-__constant__ const ull HTMSK = HTSZ - 1;
+__constant__ const int d_d = 2;
+__constant__ const ull d_q = 65521;
+__constant__ const int d_p = 1024;
+__constant__ const ull d_n = 1 << 18;
+__constant__ const ull d_m = 13;
+__constant__ const ull d_mps[] = {7, 31, 127, 8191, 131071, 524287};
+__constant__ const int d_masksz = 6;
+__constant__ const ull d_masks[] = {7, 248, 32512, 268402688, 35184103653376ull, 18446708889337462784ull};
+__constant__ const ull d_nmasks[] = {18446744073709551608ull, 18446744073709551367ull, 18446744073709519103ull, 18446744073441148927ull, 18446708889605898239ull, 35184372088831ull};
+__constant__ const ull d_shifts[] = {3, 5, 7, 13, 17, 19};
+__constant__ const ull d_cumShifts[] = {0, 3, 8, 15, 28, 45};
+__constant__ const ull d_ds[] = {2, 3, 5, 11, 13, 17};
+__constant__ const ull d_HTSZ = 1 << 18;
+__constant__ const ull d_HTMSK = HTSZ - 1;
+const int h_d = 2;
+const ull h_q = 65521;
+const int h_p = 1024;
+const ull h_n = 1 << 18;
+const ull h_m = 13;
+const ull h_mps[] = {7, 31, 127, 8191, 131071, 524287};
+const int h_masksz = 6;
+const ull h_masks[] = {7, 248, 32512, 268402688, 35184103653376ull, 18446708889337462784ull};
+const ull h_nmasks[] = {18446744073709551608ull, 18446744073709551367ull, 18446744073709519103ull, 18446744073441148927ull, 18446708889605898239ull, 35184372088831ull};
+const ull h_shifts[] = {3, 5, 7, 13, 17, 19};
+const ull h_cumShifts[] = {0, 3, 8, 15, 28, 45};
+const ull h_ds[] = {2, 3, 5, 11, 13, 17};
+const ull h_HTSZ = 1 << 18;
+const ull h_HTMSK = HTSZ - 1;
 struct testcase {
 private:
     string input_str;
@@ -41,7 +55,7 @@ private:
     char* StringGeneration(int sz) {
         char* ret = new char[sz + 1];
         mt19937 generator{ random_device{}() };
-        uniform_int_distribution<int> distribution{ 'a', 'a' + d - 1 };
+        uniform_int_distribution<int> distribution{ 'a', 'a' + h_d - 1 };
         string rand_str(sz, '\0');
         for (auto& dis : rand_str)
             dis = distribution(generator);
