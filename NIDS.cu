9ea347c23e4714ab41cca4453d3e122d015e79ec
Anas Abbas <anastorky2015@gmail.com>
2022-06-14 05:53:16 +0200

    trying to find pattern without hashtable

--- NIDS.cu
+++ NIDS.cu
@@ -22,7 +22,7 @@ CachingDeviceAllocator g_allocator(true);
 #define ull unsigned long long
 const ull h_q = 65521;
 const int h_p = 1024;
-const ull h_n = 1 << 15;
+const ull h_n = 1 << 20;
 const ull h_m = 13;
 const ull h_mps[] = {7, 31, 127, 8191, 131071, 524287};
 const int h_masksz = 6;
@@ -33,7 +33,7 @@ const ull h_HTSZ = 1 << 18;
 const int h_d = 2;
 __constant__ const int d_d = 2;
 __constant__ const ull d_q = 65521;
-__constant__ const ull d_n = 1 << 15;
+__constant__ const ull d_n = 1 << 20;
 __constant__ const ull d_m = 13;
 __constant__ const ull d_mps[] = {7, 31, 127, 8191, 131071, 524287};
 __constant__ const int d_masksz = 6;
@@ -288,7 +288,13 @@ __global__ void FindMatchesNew(ull* d_prefixSum, ull* d_lookupTable, int* d_cont
             tmp = (tmp & d_mps[k]) + (tmp >> d_shifts[k]);
             hash |= (tmp >= d_mps[k] ? tmp - d_mps[k] : tmp) << d_cumShifts[k]; 
         }
-        
+        for(int i = 0; i < d_p; i++){
+            if (hash == d_patternHashes[i]) {
+                d_output[j] = i;
+                return;
+            }
+        }
+        /*
         while (d_controlArray[hash & d_HTMSK]) {
             if (hash == d_patternHashes[d_hashTable[hash & d_HTMSK]]) {
                 d_output[j] = d_hashTable[hash & d_HTMSK];
@@ -296,6 +302,7 @@ __global__ void FindMatchesNew(ull* d_prefixSum, ull* d_lookupTable, int* d_cont
             }
             hash = (hash == d_HTMSK) ? 0: hash + 1;
         }
+        */
     }
 }
 class PaperImplementation{
