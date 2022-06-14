837af358da185de5956b2b8ad090ee6bb68faa22
Anas Abbas <anastorky2015@gmail.com>
2022-06-14 05:03:07 +0200

    small fix for debugger

--- NIDS.cu
+++ NIDS.cu
@@ -430,8 +430,8 @@ private:
         cudaDeviceSynchronize();
         // debug
         ull * h_a = new ull[h_n];
-        CubDebugExit(cudaMemcpy(h_a, d_a, sizeof(int) * h_p, cudaMemcpyDeviceToHost));
-        cout << h_a[h_n - 20] << " " << string(g_h_data).substr(0, 50) << endl;
+        CubDebugExit(cudaMemcpy(h_a, d_a, sizeof(ull) * h_n, cudaMemcpyDeviceToHost));
+        cout << h_a[h_n - 20] << " " << string(g_h_data).[h_n - 20] << endl;
         cout << "loop completed" << endl;
         //end
         return d_a;
