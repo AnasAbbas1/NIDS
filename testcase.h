9e7d873eba5ac695885505eee9fe87a269428525
Anas Abbas <anastorky2015@gmail.com>
2022-06-15 04:37:05 +0200

    resolving more syntax errors

--- testcase.h
+++ testcase.h
@@ -17,14 +17,16 @@ private:
     }
     void WriteData(char* data) {
         ofstream myfile;
-        myfile.open(string(output_path + "data.txt").c_str);
+        string data_file_name = "data.txt";
+        myfile.open(string(output_path + data_file_name).c_str);
         myfile << string(data);
         myfile.close();
     }
     void WritePatterns(char * patterns) {
         set<string>st;
         ofstream myfile;
-        myfile.open(string(output_path + "patterns.txt").c_str);
+        string pattern_file_name = "patterns.txt";
+        myfile.open(string(output_path + pattern_file_name).c_str);
         for (int patternIndex = 0; patternIndex < h_p; patternIndex++) {
             string pattern = "";
             for (int i = patternIndex * h_m; i < patternIndex * h_m + h_m; i++)
