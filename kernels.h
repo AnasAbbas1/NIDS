3afa1cc27770586038ff64bb0505d8c920254c86
Anas Abbas <anastorky2015@gmail.com>
2022-06-20 16:17:07 +0200

    bug should be fixed

--- kernels.h
+++ kernels.h
@@ -69,7 +69,7 @@ __global__ void CalculateHashesNew(ull* d_a, char* d_data, ull* d_lookupTable) {
 }
 __global__ void FindMatches(int* d_prefixSum, char* d_data, char* d_patterns, int* d_lookupTable, int* d_controlArray, int* d_hashTable, int* d_output) {
     ull j = blockIdx.x * blockDim.x + threadIdx.x;
-    if (j + d_m - 1 <= d_n) {
+    if (j + d_m - 1 < d_n) {
         int hash = ((((ull)(d_prefixSum[j + d_m - 1] - (j ? d_prefixSum[j - 1] : 0)) + d_q) % d_q) * (ull)d_lookupTable[(d_m + ((d_n - j + d_q - 2ll) / (d_q - 1ll)) * (d_q - 1ll) - d_n + j) % (d_q - 1ll)]) % d_q;
         while (d_controlArray[hash]) {
             int patternIndex = d_hashTable[hash];
@@ -90,7 +90,7 @@ __global__ void FindMatches(int* d_prefixSum, char* d_data, char* d_patterns, in
 }
 __global__ void FindMatchesNew(ull* d_prefixSum, ull* d_lookupTable, int* d_controlArray, int* d_hashTable, int* d_output, ull* d_patternHashes) {
     ull j = blockIdx.x * blockDim.x + threadIdx.x;
-    if (j + d_m - 1 <= d_n) {
+    if (j + d_m - 1 < d_n) {
         ull hash = 0;
         for(int k = 0; k < d_masksz; k++){
             ull tmp = (d_prefixSum[j + d_m - 1] & d_masks[k]) >> d_cumShifts[k];
