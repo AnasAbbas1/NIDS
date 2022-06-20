c8468b57cc4e9ed524c351bd5f6ac16c04f99318
Anas Abbas <anastorky2015@gmail.com>
2022-06-20 16:30:59 +0200

    increasing size even more

--- constants.h
+++ constants.h
@@ -22,7 +22,7 @@ CachingDeviceAllocator g_allocator(true);
 #define ull unsigned long long
 const ull h_q = 65521;
 const int h_p = 1024;
-const ull h_n = 1 << 25;
+const ull h_n = 1 << 27;
 const ull h_m = 10;
 const ull h_mps[] = {7, 31, 127, 8191, 131071, 524287};
 const int h_masksz = 6;
